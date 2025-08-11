f990fd9dd621217bd7752fcf52fe9242217cc246
diwakar1593 <diwakar200kumar@gmail.com>
2025-08-11 10:57:35 +0530

    setter injection

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/spring/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.spring.springcoredemo.rest;
+
+import com.spring.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for constructor injection
+    @Autowired
+    public void DemoController(@Qualifier("baseballCoach") Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
