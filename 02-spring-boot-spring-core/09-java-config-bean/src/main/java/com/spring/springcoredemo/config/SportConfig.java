bc9aa4d6edb9bd744e78c16085f23d856cdee44f
diwakar1593 <diwakar200kumar@gmail.com>
2025-08-17 19:28:45 +0530

    java bean config

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/spring/springcoredemo/config/SportConfig.java
@@ -0,0 +1,15 @@
+package com.spring.springcoredemo.config;
+
+import com.spring.springcoredemo.common.Coach;
+import com.spring.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfig {
+
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach();
+    }
+}
