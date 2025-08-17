c915bc5a371e7f53f31ab9a090d6490e14038ca6
diwakar1593 <diwakar200kumar@gmail.com>
2025-08-17 13:25:54 +0530

    bean scope

--- 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/spring/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/spring/springcoredemo/rest/DemoController.java
@@ -11,15 +11,25 @@ public class DemoController {
 
     // define a private field for the dependency
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // define a constructor for constructor injection
     @Autowired
+    public DemoController(
+            @Qualifier("cricketCoach") Coach theCoach,
+            @Qualifier("cricketCoach") Coach theAnotherCoach ) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach); // in case of protype scope return false else in case of singleton scope retun true.
     }
 }
