0dafa726320399a5a5072fef721dbb6394966762
diwakar1593 <diwakar200kumar@gmail.com>
2025-08-21 01:06:21 +0530

    JPA Annotations

--- 03-spring-boot-hibernate-jpa-crud/01-curddemo-student/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-curddemo-student/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.springboot.cruddemo;
 
+import com.springboot.cruddemo.dao.StudentDAO;
+import com.springboot.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,65 @@ public class CruddemoApplication {
 	}
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+
         return runner-> {
-            System.out.println("Hello World!");
+            // createStudent(studentDAO);
+
+            // createMultipleStudent(studentDAO);
+
+            readStudent(studentDAO);
         };
+    }
+
+    private void readStudent(StudentDAO studentDAO) {
+
+        // create a student object
+        System.out.println("Creating new student object ...");
+        Student tempStudent = new Student("Daffy","Duck","daffy@yahoo.com");
+
+        // save the student
+        System.out.println("Saving the students ...");
+        studentDAO.save(tempStudent);
+
+        // display id of the saved student
+        int theId = tempStudent.getId();
+        System.out.println("Saved student. Generated id: " + theId);
+
+        // retrieve student based on the id: primary key
+        System.out.println("Retrieving student with id: " + theId);
+        Student myStudent = studentDAO.findById(theId);
+
+        // display student
+        System.out.println("Found the student: " + myStudent);
+    }
+
+    private void createMultipleStudent(StudentDAO studentDAO) {
+
+        // create multiple students
+        System.out.println("Creating new student object ...");
+        Student tempStudent1 = new Student("John","Doe","john@yahoo.com");
+        Student tempStudent2 = new Student("Mary","Public","mary@yahoo.com");
+        Student tempStudent3 = new Student("Bonita","Applebum","bonita@yahoo.com");
+
+        // save the student objects
+        System.out.println("Saving the students ...");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+
+        // create the student object
+        System.out.println("Creating new student object ...");
+        Student tempStudent = new Student("Paul","Doe","paul@yahoo.com");
+
+        // save the student object
+        System.out.println("Saving the student ...");
+        studentDAO.save(tempStudent);
+
+        // display id of the saved student
+        System.out.println("Saved student. Generated id: " + tempStudent.getId());
     }
 }
---
+++ 03-spring-boot-hibernate-jpa-crud/01-curddemo-student/src/main/java/com/springboot/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,11 @@
+package com.springboot.cruddemo.dao;
+
+import com.springboot.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+}
