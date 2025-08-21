1d45e2310d174159af71a50164b18020bf2641cf
diwakar1593 <diwakar200kumar@gmail.com>
2025-08-22 00:33:02 +0530

    Automatic table generation

--- 03-spring-boot-hibernate-jpa-crud/01-curddemo-student/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-curddemo-student/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
@@ -22,7 +22,7 @@ public class CruddemoApplication {
         return runner-> {
             // createStudent(studentDAO);
 
-            // createMultipleStudent(studentDAO);
+             createMultipleStudent(studentDAO);
 
             // readStudent(studentDAO);
 
@@ -34,7 +34,7 @@ public class CruddemoApplication {
 
             // deleteStudent(studentDAO);
 
-            deleteAllStudent(studentDAO);
+            // deleteAllStudent(studentDAO);
         };
     }
 
